fac53c307abe806cbee4a1239f26055c4508aa1d
hasilka <luciehasilova@gmail.com>
2021-04-24 13:34:51 +0200

    objednací tlačítko

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import './style.css';
 
 console.log('funguju!');
+//3.cviceni//
+const navBtnElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('#nav');
+navBtnElm.addEventListener('click', () => {
+  navElm.classList.toggle('.nav-closed');
+});
+
+const navLinkElms = document.querySelectorAll('.nav-link');
+for (let i = 0; i < navLinkElms.length; i++) {
+  navLinkElms[i].addEventListener('click', () => {
+    navElm.classList.toggle('.nav-closed');
+  });
+}
+
+//4.cvicení//
+const orderBtnElm = document.querySelector('#order-btn');
+const drinkCupElm = document.querySelector('.drink__cup');
+
+let ordered = false;
+
+orderBtnElm.addEventListener('click', () => {
+  if (!ordered) {
+    drinkCupElm.classList.add('drink__cup--selected');
+    orderBtnElm.textContent = 'Zrušit';
+    ordered = true;
+  } else {
+    drinkCupElm.classList.remove('drink__cup--selected');
+    orderBtnElm.textContent = 'Objednat';
+    ordered = false;
+  }
+});
